89a661066d52f3101adebe268cd11072807e1884
Istgabrie;JT <gabrielasimbaya82@gmail.com>
2022-07-15 16:41:03 -0500

    Agregando numpy

--- Bibliotecas/Numpy/1.1_Introduccion.py
+++ Bibliotecas/Numpy/1.1_Introduccion.py
@@ -1,45 +1,126 @@
-# La biblioteca numpy se utiiza para usar arreglos y matrices en python
+# Numpy
+# Es un paquete para computo cientifico
+# Los utilizamos para crear vectores y matrices (ndarray)
+# Podemos hacer calculos matematicos y cientificos
+# Es rapido y eficiente
 
-import numpy as np # Simplificamos la palabra numpy por np
+import numpy as np
 
-# 1.- Creacion de un Array
-    # invocamos la biblioteca numpy con su alias np para declarar el metodo array.
+# 1.- Formas de crear arreglos
+# A partir de un lista
 
-vector = np.array([1, 2, 3, 4, 5])
-print (vector)
+numeros_lista = [1, 2, 3, 4, 5, 6]
+numeros_array = np.array(numeros_lista)
+print (numeros_array)
 
-# 2.- Imprimir o acceder a los elemtnos del array
-    # Usamos los corchetes con la posicion del elemento
+# Crear arreglos de dos dimensiones
+numeros_dos = np.array([1, 3, 4], [12, 6, 7])
+print (numeros_dos)
 
-print (vector[0])
-print (vector[2])
-print (vector[4])
+# Crear arreglos con tipos de datos definidos
+numeros_tres = np.array([23, 5, 4], dtype='int16')
+print (numeros_dos)
 
-# 3.- Transformar los tipos de datos
-    # utilizamos el metodo astype para transformar los datos de un array al tipo de dato
-    # que queramos.
+# 2.- Crear arreglos inicializados
+# Inicializamos un arreglo con 0
 
-print (vector.astype(str))
+ceros = np.zeros(10)
+print (ceros)
 
-# 4.- Operaciones con vectores
-    # Todas las operacione se hacen con las mismas posiciones del ambos arreglos por ejemplo:
-    # 5 + 3, 6 - 4; estos son elemtnos de los arrays a y b de abajo y que corresponden a las 
-    # posiciones 0 y 1 de ambos arrays
+# 3.- Crear arreglos con numeros
+# Agregamos numeros del 0 al 10
 
-a = np.array([5, 6, 7, 8, 9, 10])
-b = np.array([3, 4, 1, 10, 5, 2])
-print (a + b) 
-print (a - b) 
-print (a * b) 
-print (a > b)
+inicio = np.arange(10)
+print (inicio)
 
-# 5.- Metodos con arrays
+# Inica desde 0 =, termina antes dek 29 y va a r=ir de dos en dos
+pares = np.arange(0,20, 2)
+print (pares)
 
-vector2 = np.array([12, 34, 0, 120, 23])
-print (vector2.argmax()) # Retorna la posicion del maximo elemtno
+# Para crear arreglos bidimensionales
+# Crea un arreglo bidimensional de dos filas y cinco columnas
+pares.reshape(2 , 5)
+print (pares)
 
-print (vector2.sum()) # Suma todos los elemtnos
-print (vector2.prod()) # Multiplica todos los elemtnos
+# 4.- Operaiones entre arrays
+
+impares = pares + 1
+print (impares) # Suma a todos los elemtnos 1
+
+impares * 100
+impares - pares
+impares / pares # Python define la division como cero de forma indefinida
+
+# 5.- Metodos con operaciones
+
+# Suma todos loes slemetnos del arreglo
+impares.sum() 
+
+# Saca el promedio dek arregki
+impares.mean()
+
+# Saca la varianza del arreglo
+impares.var()
+
+# Ordenar de menor a mayor un arreglo
+notas = np.array([23, 34, 0, 1, 200, 45, 54, 12, 5, 23, 50])
+np.sort(notas)
+
+# Odeerna de mayor a menor
+-np.sort(-notas)
+
+# Obtener las dimensiones de un arreglo
+print (pares.ndim)
+print (notas.ndim)
+
+# Obetner el tamano del arreglo 
+print (pares.size)
+
+# Llenar todo un arreglo de unos
+unos = np.ones((4, 2), dtype='float32')
+print (unos)
+
+# LLenar todo un arreglo con un mismo numero
+cincos = np.full((2,2), 5)
+print (cincos)
+
+# LLenar un arreglo de forma ramdon
+random = np.random.rand(4,2)
+print (random)
+
+# 6.- Forma de seleccionar elemtnos
+
+A = np.arange(0, 20, 2).reshape(2, 5)
+print (A[0,0]) # Cogemos una fila y una columna
+
+# para sekecionar todo una fila
+print (A[1, :])
+
+# Trabaajr con fikters
+notas[notas < 20]  # Guarda los elementos que son menores que 20 (dentro de los corchetes se cumple la codndicion)
+print (notas)
+ 
+# 7.- Formas de copiar un arreglo
+# Cunado asifnamos directamente el arreglo, estamos redirigiendo a esa variable a la misma direccion de memoira
+# Que tiene el arreglo original
+# POr tanto cuando cambiamos un elemento del arreglo nuevo tambien fecta al arreglo original
+
+f = np.array([1, 3, 4])
+g = f
+g[0] = 100
+print (f)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
