056f28488fcf520092226203ddfe828405a9b146
Istgabrie;JT <gabrielasimbaya82@gmail.com>
2022-08-05 19:50:39 -0500

    Agregando Pandas

--- Ejericicios/Basicos/Ejercicio 2.2.py
+++ Ejericicios/Basicos/Ejercicio 2.2.py
@@ -3,13 +3,32 @@ son impares.C="isa,4,w,5,6,7,8,isa" '''
 
 C="isa,4,w,5,6,7,8,isa"
 
-def cogerNumeros():
-    numeros = C.split(","),
-    _, b, _ = numeros
-#    print(a)
-    print(c)
- #   print(b)
+numeros = C.split(',')
 
-cogerNumeros() 
+a, *b, c =  numeros
+
+print(b)
+
+b.remove('w')
+
+lista = []
+
+try:
+    lista = list(map(int, b))
+except ValueError as e:
+    raise e
+
+print (lista)
+
+i = 0
+j = 0
+for n in lista:
+    if n % 2 == 0:
+        i += 1        
+    else:
+        j += 1
+
+print(f"{i}, {j}")
+
 
 
